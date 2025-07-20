05284be54a4e58aa5128939df2411ba420c2e72b
srinivaspradhan64 <srinivaspradhan@gmail.com>
2025-07-20 17:00:44 -0400

    Add S3 interactor code

--- src/core/s3/s3Interactor.mjs
+++ src/core/s3/s3Interactor.mjs
@@ -6,3 +6,38 @@ import {
     PutObjectCommand
 } from "@aws-sdk/client-s3";
 
+const Region = process.env.AWS_DEFAULT_REGION;
+const S3Bucket = process.env.S3_BUCKET;
+
+const client = new S3Client({ region: Region });
+
+export const PutS3Item = async function PutS3Item(Item, ObjectKey, Bucket = S3Bucket) {
+    const command = new PutObjectCommand({
+        "Body": Item,
+        "Bucket": Bucket,
+        "ObjectKey": ObjectKey
+    });
+    try {
+        const results = await client.send(command);
+        return results
+        
+    } catch (error) {
+        console.log(error)
+        return error.name
+    }
+};
+
+export const GetS3Item = async function GetS3Item(ObjectKey, Bucket = S3Bucket) {
+    const command = new GetObjectCommand({
+        "Bucket": Bucket,
+        "Key": ObjectKey
+    });
+    try {
+        const results = await client.send(command);
+        return results
+        
+    } catch (error) {
+        console.log(error)
+        return error.name
+    }
+};
